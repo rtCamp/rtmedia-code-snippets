e3ec443d2d6fb1ef3ec63c5c9cb9e4d3e5db4d71
bunty <bn.bhandari90@gmail.com>
2017-05-19 07:35:12 +0530

    Rename function

--- media/rtm_tabular_format_media.php
+++ media/rtm_tabular_format_media.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Add this function to theme's functions.php
- * Show media in tabular format.
+ * Show media in tabular format when document view is enabled as a table.
  *
  * @param  bool $status  Status of function.
  * @return bool          Returns true tabular format is enabled from rtMedia settings.
  */
-function rtmedia_enable_tabular_view( $status ) {
+function rtm_enable_tabular_view( $status ) {
 	global $rtmedia_query;
 	if ( function_exists( 'rtm_is_document_table_view_enabled' ) && rtm_is_document_table_view_enabled() && isset( $rtmedia_query->action_query->action ) && 'edit' !== $rtmedia_query->action_query->action ) {
 		if ( ( function_exists( 'is_rtmedia_album' ) && is_rtmedia_album() ) || ( function_exists( 'is_rtmedia_gallery' ) && is_rtmedia_gallery() ) ) {
@@ -15,4 +15,4 @@ function rtmedia_enable_tabular_view( $status ) {
 	}
 	return $status;
 }
-add_filter( 'rtmedia_other_files_is_document_tab', 'rtmedia_enable_tabular_view', 99 );
+add_filter( 'rtmedia_other_files_is_document_tab', 'rtm_enable_tabular_view', 99 );
