510befedb3207623d08bbe46f1e73b067cac2bef
bunty <bn.bhandari90@gmail.com>
2017-10-09 15:44:38 +0530

    Update function for showing gallery

--- media/profile/rtmc_show_gallary.php
+++ media/profile/rtmc_show_gallary.php
@@ -6,7 +6,6 @@
  *
  * Two actions are given:
  *  - bp_before_profile_field_content => For showing above prfile fileds.
- *  - bp_before_member_header => For showing above cover photo.
  *
  * Chose action according to your requirement and remove action which is not necessary to you.
  */
@@ -21,4 +20,3 @@ function rtmc_show_gallary() {
 	echo do_shortcode( '[rtmedia_gallery global="true" media_author="' . $user_id . '"]' );
 }
 add_action( 'bp_before_profile_field_content', 'rtmc_show_gallary' ); // For showing above profile fields.
-add_action( 'bp_before_member_header', 'rtmc_show_gallary' ); // For showing above cover photo.
