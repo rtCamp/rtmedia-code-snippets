ef13eba7d40ae3ca8d81dc93d94ced509614f186
bunty <bhargav.bhandari@rtcamp.com>
2017-11-01 12:53:01 +0530

    Filter for sending failed notification of transcoding videos

---
+++ transcoder/rtmc_nofity_to_custom_id.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Send failed notification mail to particular id.
+ * By this filter two mail will be sent
+ * 1. To site administrator
+ * 2. To specified email id on line 17
+ *
+ * If you want to send mail only to administrator, then change line 17
+ * to $email_ids = array();
+ *
+ * @param array  $email_ids Array of email ids.
+ * @param string $job_id    Job id.
+ * @return array            Array of particular email ids.
+ */
+function rtmc_nofity_to_custom_id( $email_ids, $job_id ) {
+
+	$email_ids = array( 'YOUR_EMAIL_ID' ); // Set your id to whome you want to send your failed notification.
+
+	return $email_ids;
+}
+add_filter( 'rtt_nofity_transcoding_failed', 'rtmc_nofity_to_custom_id', 99, 2 );
