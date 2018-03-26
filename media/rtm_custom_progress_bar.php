eca1525e35d5e39f691dd0fdb0851eb5c7fab077
Thrijith Thankachan <thrijith13@gmail.com>
2018-03-26 18:36:17 +0530

    update code according to review

--- media/rtm_custom_progress_bar.php
+++ media/rtm_custom_progress_bar.php
@@ -12,17 +12,17 @@ if ( ! function_exists( 'rtm_custom_progress_bar' ) ) {
 	function rtm_custom_progress_bar() {
 		?>
 		<script>
-		if( typeof rtMediaHook == "object" ){   // check if rtMediaHook defined or not
+		if ( typeof rtMediaHook == "object" ) {   // check if rtMediaHook defined or not
 			rtMediaHook.register(
 				'rtm_custom_progress_bar_content', // hook id here
 				function ( args ) { // your function here
 					var progress_status = args[0].percent;
-					var file_id = args[0].id;
-					jQuery( '#' + file_id ).find( '.plupload_file_progress.ui-widget-header' ).html( progress_status + '%' );
+					var file_id         = args[0].id;
+					jQuery( '#' + file_id ).find( '.plupload_file_progress.ui-widget-header' ).text( progress_status + '%' );
 				}
 			);
 		}
-	</script>
+        </script>
 		<?php
 	}
 }
